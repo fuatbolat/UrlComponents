820a7458fe2d018545c9cb3ede03ab492473a3a5
fuatbolat <byfuatbolat@gmail.com>
2025-01-26 23:45:35 +0300

    last

--- QueryURL/main.swift
+++ QueryURL/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+
+
+let baseUrl = URL(string: "https://apple.com")!
+
+
+
+//MARK: - URLExtension
+
+extension URL{ func query(_ query: [String: String]) -> URL? {
+        
+    var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+    
+    urlComponents.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+    
+    return urlComponents.url
+}
+}
+
+let urlDictioınary = ["test1" : "Test" , "test2" : "Test2"]
+
+print(baseUrl.query(urlDictioınary)!)
+
 
